80b776133690a6d4781778d8e23b21292450b8c9
Jarrod Jackson <jarrod@doover.com>
2024-03-08 16:13:43 +1000

    small bug fix

--- processor/target.py
+++ processor/target.py
@@ -347,7 +347,7 @@ class target:
             self.add_to_log("cat api response " + str(msg))
             if msg is not None:
                 self.uplink_recv_channel.publish(
-                    msg_str=str(msg["Equipment"][0])
+                    msg_str=msg["Equipment"][0]
                 )
 
     def get_cat_keys(self):
